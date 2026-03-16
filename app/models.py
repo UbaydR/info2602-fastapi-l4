5798b911cd9c7688840e641b0324ba17cdd71e13
UbaydR <131396971+UbaydR@users.noreply.github.com>
2026-03-15 23:24:03 -0400

    completed Lab 4

--- app/models.py
+++ app/models.py
@@ -51,3 +51,30 @@ class Todo(SQLModel, table=True):
     
     def get_cat_list(self):
         return ', '.join([category.text for category in self.categories])
+
+class TodoCreate(SQLModel):
+    text:str
+
+class TodoResponse(SQLModel):
+    id: Optional[int] = Field(primary_key=True, default=None)
+    text:str
+    done: bool = False
+
+class TodoUpdate(SQLModel):
+    text: Optional[str] = None
+    done: Optional[bool] = None
+
+class UserCreate(SQLModel):
+    username:str
+    email: EmailStr = Field(max_length=255)
+    password: str = Field(min_length=8, max_length=128)
+
+class Token(SQLModel):
+    access_token: str
+    token_type: str
+
+class UserResponse(SQLModel):
+    id: int
+    username: str = Field(max_length=255)
+    email: EmailStr = Field(max_length=255)
+    role: str
